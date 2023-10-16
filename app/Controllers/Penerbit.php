74a087425dc397a24abb703546a889e288756cc1
Erica201122 <ericagracilamuntu201122@gmail.com>
2023-10-16 11:31:10 +0700

    tambah controller penerbit

---
+++ app/Controllers/Penerbit.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Penerbitt extends BaseController
+{
+    public function id()
+    {
+        //
+    }
+    public function penerbit()
+    {
+
+    }
+    public function kota()
+    {
+
+    }
+}
